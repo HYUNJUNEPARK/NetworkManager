cdfd6a1a509cd33bee04c8a1cc24fbb9fa744e73
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-02-03 16:44:40 +0900

    fix: 앱, 모듈 버전 문제 수정

--- NetworkManager/src/androidTest/java/com/module/networkmanager/ExampleInstrumentedTest.kt
+++ NetworkManager/src/androidTest/java/com/module/networkmanager/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.ex.app
+package com.module.networkmanager
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ex.app", appContext.packageName)
+        assertEquals("com.module.networkmanager.test", appContext.packageName)
     }
 }
